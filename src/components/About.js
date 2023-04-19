ca95fa320e0c9e38c5e5dd7d0180c9247f4f7d9e
MORGANJASON <420morganjason@gmail.com>
2023-04-19 22:31:51 +0300

    created articles

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const About = (props) => {
+  return (
+    
+       <img
+      className="avatar"
+      src= {props.image}
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About";
+import ArticleList from "./ArticleList";
+
 
 console.log(blogData);
 
@@ -8,7 +12,14 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header name = "Pablo"/>
+      <About image = "https://via.placeholder.com/215" />
+      <ArticleList title = "Morgee" preview = "Ayembeteza morgee"/>
+      alt = "blog logo"
+      />
+      <p>Welcome to Moringa!</p>
     </div>
+    
   );
 }
 
