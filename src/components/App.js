fa1fae64c64baa8450e7b316fe2a8d8f1ee03835
MORGANJASON <420morganjason@gmail.com>
2023-04-19 22:47:10 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -12,11 +12,17 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
-      <Header name = "Pablo"/>
+      <Header name = "Overreacted"/>
       <About image = "https://via.placeholder.com/215" />
-      <ArticleList title = "Morgee" preview = "Ayembeteza morgee"/>
+      <ArticleList title = "The WET Codebase" preview = "July 13,2020"/>
       alt = "blog logo"
-      />
+         <ArticleList title = "Goodbye,Clean Code" preview = "January 11,2020"/>
+      alt = "blog logo"
+      
+         <ArticleList title = "My Decade in Review" preview = "January 1,2020"/>
+      alt = "blog logo"
+      
+      
       <p>Welcome to Moringa!</p>
     </div>
     
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div>
-      <h1>Ayeeza my name is {props.name}</h1>
+      <h1>{props.name}</h1>
     </div>
   )
 }
